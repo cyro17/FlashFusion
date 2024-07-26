3b9a7c0fb9c3655790acb95da4f66369d51e16d1
Cyro <coolronitchs@gmail.com>
2024-07-27 02:54:48 +0530

    item details page final push

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -51,6 +51,15 @@ const ItemDetails = () => {
   }, [itemId]); // eslint-disable-line react-hooks/exhaustive-deps
 
   console.log(item);
+  const itemName = item?.attributes?.name;
+  const itemPrice = item?.attributes?.price;
+  const itemCategory = item?.attributes?.category;
+  const shortDescription =
+    item?.attributes?.shortDescription[0]?.children[0]?.text;
+  const itemLongDescription =
+    item?.attributes?.longDescription[0]?.children[0]?.text;
+
+  const imageUrl = `http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`;
 
   return (
     <Box width="80%" m="80px auto">
@@ -58,27 +67,22 @@ const ItemDetails = () => {
         {/* IMAGES */}
         <Box flex="1 1 40%" mb="40px">
           <img
-            alt={item?.name}
-            width="100%"
-            height="100%"
-            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            src={imageUrl}
+            alt="item details image"
             style={{ objectFit: "contain" }}
           />
         </Box>
 
-        {/* ACTIONS */}
-        <Box flex="1 1 50%" mb="40px">
+        <Box flex="1 1 50%" mb="40%">
           <Box display="flex" justifyContent="space-between">
-            <Box>Home/Item</Box>
-            <Box>Prev Next</Box>
+            <Box>Home / Item</Box>
+            <Box>Prev / Next</Box>
           </Box>
 
           <Box m="65px 0 25px 0">
-            <Typography variant="h3">{item?.attributes?.name}</Typography>
-            <Typography>${item?.attributes?.price}</Typography>
-            {/* <Typography sx={{ mt: "20px" }}>
-              {item?.attributes?.longDescription}
-            </Typography> */}
+            <Typography variant="h3">{itemName}</Typography>
+            <Typography>{itemPrice} $</Typography>
+            <Typography sx={{ mt: "20px" }}>{shortDescription} </Typography>
           </Box>
 
           <Box display="flex" alignItems="center" minHeight="50px">
@@ -86,36 +90,36 @@ const ItemDetails = () => {
               display="flex"
               alignItems="center"
               border={`1.5px solid ${shades.neutral[300]}`}
-              mr="20px"
-              p="2px 5px"
             >
               <IconButton onClick={() => setCount(Math.max(count - 1, 0))}>
                 <RemoveIcon />
               </IconButton>
-              <Typography sx={{ p: "0 5px" }}>{count}</Typography>
+              <Typography sx={{ p: "0 50x" }}> {count}</Typography>
               <IconButton onClick={() => setCount(count + 1)}>
                 <AddIcon />
               </IconButton>
             </Box>
+
             <Button
               sx={{
                 backgroundColor: "#222222",
-                color: "white",
+                color: "white ",
                 borderRadius: 0,
                 minWidth: "150px",
                 padding: "10px 40px",
               }}
               onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
             >
-              ADD TO CART
+              Add To Cart
             </Button>
           </Box>
+
           <Box>
-            <Box m="20px 0 5px 0" display="flex">
+            <Box display="flex" m="20px 0 5px 0">
               <FavoriteBorderOutlinedIcon />
               <Typography sx={{ ml: "5px" }}>ADD TO WISHLIST</Typography>
             </Box>
-            <Typography>CATEGORIES: {item?.attributes?.category}</Typography>
+            <Typography>CATEGORIES: {itemCategory} </Typography>
           </Box>
         </Box>
       </Box>
@@ -124,30 +128,28 @@ const ItemDetails = () => {
       <Box m="20px 0">
         <Tabs value={value} onChange={handleChange}>
           <Tab label="DESCRIPTION" value="description" />
-          <Tab label="REVIEWS" value="reviews" />
+          <Tab label="REVIEW" value="reviews" />
         </Tabs>
       </Box>
-      <Box display="flex" flexWrap="wrap" gap="15px">
-        {value === "description" && (
-          <div>{item?.attributes?.longDescription}</div>
-        )}
-        {value === "reviews" && <div>reviews</div>}
+      <Box>
+        {value == "description" && <div>{itemLongDescription}</div>}
+        {value == "reviews" && <div>reviews</div>}
       </Box>
 
-      {/* RELATED ITEMS */}
+      {/* Related Items */}
       <Box mt="50px" width="100%">
         <Typography variant="h3" fontWeight="bold">
           Related Products
         </Typography>
         <Box
           mt="20px"
+          columnGap="1.33%"
           display="flex"
           flexWrap="wrap"
-          columnGap="1.33%"
           justifyContent="space-between"
         >
-          {items.slice(0, 4).map((item, i) => (
-            <Item key={`${item.name}-${i}`} item={item} />
+          {items.slice(1, 5).map((item, index) => (
+            <Item key={`${item.name}-${index}`} item={item} />
           ))}
         </Box>
       </Box>
